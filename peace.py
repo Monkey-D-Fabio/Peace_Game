8c13e1c2aca60bd57f344865fe9d3cc1d1b52d38
Fabio Castro Morffi <141592488+Monkey-D-Fabio@users.noreply.github.com>
2024-10-25 18:51:48 -0400

    Update peace.py

--- peace.py
+++ peace.py
@@ -7,8 +7,8 @@ suits = ("hearts", "diamonds", "clubs", "spades")
 
 # Create a deck of cards and discard piles
 deck = [(suit,rank) for suit in suits for rank in ranks]
-pile1 = 0
-pile2 = 0
+pile1 = []
+pile2 = []
 # Shuffle the deck 
 
 random.shuffle(deck)
@@ -23,19 +23,29 @@ def card_comparison(p1_card, p2_card):
 		Hint, using the index function will make this very simple (one liner)"""
     # Your code here
     if ranks.index(p1_card[1]) > ranks.index(p2_card[1]):
-        print("P1 BEATS!!! P2")
         return 1
     elif ranks.index(p1_card[1]) < ranks.index(p2_card[1]):
-        print("P2 BEATS!!! P2")
         return 2
     return 0
-def play_round(player1_hand, player2_hand):
+def play_round(p1_card, p2_card, pile1, pile2):
     """Play a single round of the game.
 		That is, each player flips a card, and the winner is determined using the card_comparison function
 		if both players flip the same value card, call the war function
 	"""
     # Your code here
-    
+	print(f"P1 played {p1_card}")
+	print(f"P1 played {p2_card}")
+	if result == 1:
+		print("P1 BEATS!!! P2")
+		pile1.append(p1_card)
+		pile1.append(p2_card)
+	elif result == 2:
+		print("P2 BEATS!!! P1")
+		pile2.append(p1_card)
+		pile2.append(p2_card)
+	else:
+		print("WAAAAAAARRRRR")
+		war(player1_hand, player2_hand)
 def war(player1_hand, player2_hand):
     """Handle the 'war' scenario when cards are equal.
 		recall the rules of war, both players put 3 cards face down, 
@@ -43,20 +53,23 @@ def war(player1_hand, player2_hand):
 		card takes all the cards.		
 	"""
     # Your code here
-    
+    return p1_card
 def play_game():
     """Main function to run the game."""
     # Your code here
-    while len(p1_deck) > 0 and len(p2_deck) > 0:
+    while len(player1_hand) > 0 and len(player2_hand) > 0:
         p1_card, p2_card = player1_hand.pop(0), player2_hand.pop(0)
         result = card_comparaison(p1_card, p2_card)
-        if result == 1:
-		pile1 +=2
-	elif result == 2:
-		pile2 += 2
-	elif not(result):
-		war(player1_hand, player2_hand)
-				
+        play_round(p1_card, p2_card, pile1, pile2)
+    	if len(player1_hand) < 0 or len(player2_hand) < 0:
+		# recycling pile into playerhand
+		player1_hand = pile1
+		pile1 = []
+		random_shuffle(player1_hand)
+		player2_hand = pile2
+		pile2 = []
+		random_shuffle(player2_hand)
+		
 # Call the main function to start the game
 play_game()
 
