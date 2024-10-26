cc0f565ab2729d5de60024be931736de4b417051
Fabio Castro Morffi <141592488+Monkey-D-Fabio@users.noreply.github.com>
2024-10-25 21:06:52 -0400

    Update peace.py

--- peace.py
+++ peace.py
@@ -12,9 +12,9 @@ pile2 = []
 # Shuffle the deck 
 
 random.shuffle(deck)
-
-# Split the deck into two hands
+# Split the deck into two hands 
 player1_hand, player2_hand = deck[:26], deck[26:]
+
 def card_comparison(p1_card, p2_card):
     """This is the logic that compares two cards to find the stronger card
 		Return 1 if player 1's card is strong, 2 for player 2
@@ -27,49 +27,66 @@ def card_comparison(p1_card, p2_card):
     elif ranks.index(p1_card[1]) < ranks.index(p2_card[1]):
         return 2
     return 0
-def play_round(p1_card, p2_card, pile1, pile2):
+def play_round(result, p1_card, p2_card, pile1, pile2):
     """Play a single round of the game.
 		That is, each player flips a card, and the winner is determined using the card_comparison function
 		if both players flip the same value card, call the war function
 	"""
     # Your code here
-	print(f"P1 played {p1_card}")
-	print(f"P1 played {p2_card}")
-	if result == 1:
-		print("P1 BEATS!!! P2")
-		pile1.append(p1_card)
-		pile1.append(p2_card)
-	elif result == 2:
-		print("P2 BEATS!!! P1")
-		pile2.append(p1_card)
-		pile2.append(p2_card)
-	else:
-		print("WAAAAAAARRRRR")
-		war(player1_hand, player2_hand)
-def war(player1_hand, player2_hand):
+    print(f"P1 played {p1_card}")
+    print(f"P2 played {p2_card}")
+    if result == 1:
+    	print("P1 BEATS!!! P2")
+    	pile1.append(p1_card)
+    	pile1.append(p2_card)
+    elif result == 2:
+    	print("P2 BEATS!!! P1")
+    	pile2.append(p1_card)
+    	pile2.append(p2_card)
+    else:
+    	print("WAAAAAAARRRRR")
+    	#pile2.append(p1_card)
+    	#pile2.append(p2_card)
+    	war(p1_card, p2_card)
+def war(p1_card, p2_card, player1_hand, player2_hand):
     """Handle the 'war' scenario when cards are equal.
 		recall the rules of war, both players put 3 cards face down, 
 		then both players flip face up a 4th card. The player with the stronger
 		card takes all the cards.		
 	"""
-    # Your code here
-    return p1_card
-def play_game():
+	while ranks.index(p1_card[1]) == ranks.index(p2_card[1]):
+    	war_extra_cards = []
+        # Your code here
+        if len(player1_hand) < 4:
+            cards_left = 4 - len(player1_hand)
+            war_extra_cards += player1_hand
+            player1_hand = pile1
+            random.shuffle(player1_hand)
+        if len(player2_hand) < 4:
+            cards_left = 4 - len(player2_hand)
+            war_extra_cards += player2_hand
+            player2_hand = pile2
+            random.shuffle(player2_hand)
+        
+def play_game(player1_hand, player2_hand, pile1, pile2):
     """Main function to run the game."""
     # Your code here
     while len(player1_hand) > 0 and len(player2_hand) > 0:
         p1_card, p2_card = player1_hand.pop(0), player2_hand.pop(0)
-        result = card_comparaison(p1_card, p2_card)
-        play_round(p1_card, p2_card, pile1, pile2)
-    	if len(player1_hand) < 0 or len(player2_hand) < 0:
-		# recycling pile into playerhand
-		player1_hand = pile1
-		pile1 = []
-		random_shuffle(player1_hand)
-		player2_hand = pile2
-		pile2 = []
-		random_shuffle(player2_hand)
+        result = card_comparison(p1_card, p2_card)
+        play_round(result, p1_card, p2_card, pile1, pile2)
+        if len(player1_hand) == 0 or len(player2_hand) == 0:
+    	    player1_hand = pile1
+    	    pile1 = []
+    	    random.shuffle(player1_hand)
+    	    player2_hand = pile2
+    	    pile2 = []
+    	    random.shuffle(player2_hand)
+    	    print("Reshuffled")
+    if not(player1_hand):
+        print("P2's victory")
+    else:
+        print("P1's victory")
 		
 # Call the main function to start the game
-play_game()
-
+play_game(player1_hand, player2_hand, pile1, pile2) 
